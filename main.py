8ac508c9a3b5f98921650f3ab2e4e21beabf9f43
Dag Einar Monsen <me@dag.im>
2013-09-11 17:39:13 +0200

    Read users file.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 # read files in data
+# read users
+file = open('./data/users.dat', 'r')
+for line in file:
+    print line
+
+# read movies
+# read ratings
+
 
 # do collaborative filtering
 
