208ad116da3df51c3142ab373df7b41d32b54e14
Dag Einar Monsen <me@dag.im>
2013-09-26 09:47:54 +0200

    Read data files and parse each line.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
+users = []
+movies = []
+ratings = []
+
 # read files in data
 # read users
 file = open('./data/users.dat', 'r')
 for line in file:
-    print line
+    users.add(MovieLensParser.parseUser())
 
+file = open('./data/movies.dat', 'r')
+for line in file:
+    movies.add(MovieLensParser.parseMovie())
+
+for line in file:
+    ratings.add(MovieLensParser.parseRating(users, movies))
 # read movies
 # read ratings
 
