712bc54a7c3e51a64f29a02722929d44764fb7e3
Patrick Romstad <promstad1@gmail.com>
2013-09-26 16:00:07 +0200

    Most of kNN finished. Small bug thou, it does not finish

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 from src import Reader
+from src import SimpleKNN as kNN
 
 #
 (users, movies, ratings) = Reader.read_data('./data')
 
-print "##################################"
-print "Loaded MovieLens data"
-print "Users: %d" % (len(users))
-print "Movies: %d" % (len(movies))
-print "Ratings: %d" % (len(ratings))
-print "##################################"
+# print "##################################"
+# print "Loaded MovieLens data"
+# print "Users: %d" % (len(users))
+# print "Movies: %d" % (len(movies))
+# print "Ratings: %d" % (len(ratings))
+# print "##################################"
 
 # make recommendations
+
+matrix = kNN.compute_recommendations(users)
+
+# ???????
 
 # Profit!
---
+++ src/SimpleKNN.py
@@ -0,0 +1,37 @@
+## Simple kNN algorithm
+
+# Similarity matrix between users 
+import numpy as np
+import math
+
+
+def calculate_dot_product(user_u_rating, user_v_rating):
+	sum = 0
+	for movie_u, rating_u in user_u_rating.iteritems():
+		for movie_v, rating_v in user_v_rating.iteritems():
+			if movie_u == movie_v:
+				sum = sum + rating_u.value * rating_v.value
+	return sum
+
+def calculate_abs_vector(user_rating):
+	sum = 0
+	for key, rating in user_rating.iteritems():
+		sum = sum + rating.value ** 2
+
+	return math.sqrt(sum)
+
+# User.ratings : hashmap movie_id:tuple(movie,rating)
+# need only to do computing on movies users both have rated, since the dotproduct of other movies are 0
+def compute_similarity_between_users(u,v):
+		
+	return 1 - calculate_dot_product(u.ratings, v.ratings)/(calculate_abs_vector(u.ratings)*calculate_abs_vector(v.ratings))
+
+def compute_recommendations(users):
+	sim = np.zeros(shape=(len(users),len(users)))
+
+	for key_u, user_u in users.iteritems():
+		for key_v, user_v in users.iteritems():
+			compute_similarity_between_users(user_u, user_v)
+
+
+	return sim
