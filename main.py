078567a4a9263064ae736ada69604848a01f7600
Patrick Romstad <promstad1@gmail.com>
2013-09-26 10:46:18 +0200

    Fixed documentation

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ lines = list(reader)
 for line in lines:
     users.append(MovieLensParser.parse_user(line))
 
+# read movies
 reader = csv.reader(open('./data/movies.dat', 'rb'))
 lines = list(reader)
 for line in lines:
     movies.append(MovieLensParser.parse_movie(line))
 
+# read ratings
 reader = csv.reader(open('./data/ratings.dat', 'rb'))
 lines = list(reader)
 for line in lines:
     ratings.append(MovieLensParser.parse_rating(users, movies))
-# read movies
-# read ratings
+
 
 # do collaborative filtering
 
