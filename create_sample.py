fae36639e230409978470dfff958f9aec9dc8302
Dag Einar Monsen <me@dag.im>
2013-09-27 13:06:41 +0200

    Make script for creating sample data subsets useful in debugging.

---
+++ create_sample.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from src.data import Reader, Writer
+import src.path
+from os.path import join
+import random
+
+#
+(users, movies, ratings) = Reader.read_data(src.path.get('data/movielens-1m'))
+
+print "##################################"
+print "Loaded MovieLens data"
+print "Users: %d" % (len(users))
+print "Movies: %d" % (len(movies))
+print "Ratings: %d" % (len(ratings))
+print "##################################"
+
+# get random sample of 100 users
+users_sample = random.sample(users.values(), 100)
+
+ratings_sample = []
+
+for user in users_sample:
+    ratings_sample += user.ratings.values()
+
+movies_sample = {}
+
+for rating in ratings_sample:
+    movies_sample[rating.movie.id] = rating.movie
+
+
+path_sample = src.path.get('data/sample100')
+
+Writer.write_users(join(path_sample, 'users.dat'), users_sample)
+Writer.write_movies(join(path_sample,'movies.dat'), movies_sample.values())
+Writer.write_ratings(join(path_sample,'ratings.dat'), ratings_sample)
+
+# Profit!
