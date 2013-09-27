f273eaa33f123df5350c3016d66e5536ed092407
Patrick Romstad <promstad1@gmail.com>
2013-09-27 10:51:58 +0200

    Added labels on functions

--- src/SimpleKNN.py
+++ src/SimpleKNN.py
@@ -4,7 +4,7 @@
 import numpy as np
 import math
 
-
+# calculates the dot product of two users
 def calculate_dot_product(user_u_rating, user_v_rating):
 	sum = 0
 	for movie_u, rating_u in user_u_rating.iteritems():
@@ -13,6 +13,7 @@ def calculate_dot_product(user_u_rating, user_v_rating):
 				sum = sum + rating_u.value * rating_v.value
 	return sum
 
+# calculates the absolute value of a vector (a user/item)
 def calculate_abs_vector(user_rating):
 	sum = 0
 	for key, rating in user_rating.iteritems():
@@ -23,15 +24,19 @@ def calculate_abs_vector(user_rating):
 # User.ratings : hashmap movie_id:tuple(movie,rating)
 # need only to do computing on movies users both have rated, since the dotproduct of other movies are 0
 def compute_similarity_between_users(u,v):
-		
+	
+	# cosine similarity function
 	return 1 - calculate_dot_product(u.ratings, v.ratings)/(calculate_abs_vector(u.ratings)*calculate_abs_vector(v.ratings))
 
 def compute_recommendations(users):
 	sim = np.zeros(shape=(len(users),len(users)))
 
+	# Compute similarities between users
 	for key_u, user_u in users.iteritems():
 		for key_v, user_v in users.iteritems():
 			compute_similarity_between_users(user_u, user_v)
+
+	print 'Finished'
 
 
 	return sim
