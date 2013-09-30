04f5af5d84cf3103dcb5ff2cf7cda78b2151c58e
Dag Einar Monsen <me@dag.im>
2013-09-30 16:49:10 +0200

    Move crossvalidate to PredictiveAccuracy module and continue implementation.

--- src/evaluation/__init__.py
+++
@@ -1,4 +0,0 @@
-
-def cross_validate():
-    """ do a cross validation on recommendation algorithm """
-    return
--- src/evaluation/predictiveAccuracy.py
+++ src/evaluation/predictiveAccuracy.py
@@ -1,4 +1,39 @@
 from math import sqrt
+from collections import deque
+
+def _chunks(l, n):
+    """ Yield successive n-sized chunks from l. """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+def cross_validate(user, items, recommender):
+    """ do a cross validation on recommendation algorithm """
+    folds = 10
+
+    # divide items into 10 parts
+    folds = _chunks(items, len(items)/folds)
+    queue = deque(folds)
+
+    for i in range(1,folds):
+        # for each, take it out, train on the rest (load to memory)
+        fold = queue.popleft()
+
+        sum_err = 0
+        # predict rating for each item in the fold
+        for item_id, item in fold:
+            rating = recommender.get_rating(user, item)
+            sum_err += root_mean_squared_error()
+
+        # avg
+        avg = sum_err / len(fold)
+
+        queue.append(fold)
+
+    # return average error
+
+
+
+    return avg
 
 def mean_absolute_error(ratings, range):
     """ measures average deviation (error) in the predicted rating compared to the true
