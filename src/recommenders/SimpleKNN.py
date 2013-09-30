d84a32be2218ed09ad044a0ce0d312e901920021
Dag Einar Monsen <me@dag.im>
2013-09-30 15:14:45 +0200

    Fix typo: denomiator -> denominator

--- src/recommenders/SimpleKNN.py
+++ src/recommenders/SimpleKNN.py
@@ -45,22 +45,21 @@ def make_prediction(similarity_vector, users, user, neighbors, movie):
     For this function, N will be 10, i.e. the ten closest neighbours
     """
     numerator = 0
-    denomiator = 0
+    denominator = 0
 
     for neighbor in neighbors:
         neighbor_object = users[neighbor]
 
         # check if user has rated the movie, it not, rating is set to 0
-        if ( neighbor_object.ratings.has_key(movie) ):
+        if movie in neighbor_object.ratings:
             neighbor_movie_rating = neighbor_object.ratings[movie].value
         else:
             neighbor_movie_rating = 0
 
-        numerator += similarity_vector[neighbor_object.id] * (
-        neighbor_movie_rating - neighbor_object.get_rating_average())
-        denomiator += similarity_vector[neighbor_object.id]
+        numerator += similarity_vector[neighbor_object.id] * (neighbor_movie_rating - neighbor_object.get_rating_average())
+        denominator += similarity_vector[neighbor_object.id]
 
-    return user.get_rating_average() + numerator / denomiator
+    return user.get_rating_average() + numerator / denominator
 
 
 # returns a dictionary user_id:similarity_measure
