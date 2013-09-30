b07fc9d581d85e2e6dea2a3f86c5acfb0d095a9b
Dag Einar Monsen <me@dag.im>
2013-09-30 15:19:17 +0200

    Change code style.

--- src/recommenders/SimilarityMetrics.py
+++ src/recommenders/SimilarityMetrics.py
@@ -6,41 +6,45 @@ these users
 # Similarity matrix between users 
 import math
 
-# calculates the dot product of two users
 def calculate_dot_product(user_u_rating, user_v_rating):
+    """ calculates the dot product of two users """
+
     sum = 0
     for movie_u, rating_u in user_u_rating.iteritems():
         for movie_id_v, rating_v in user_v_rating.iteritems():
             if movie_u == movie_id_v:
-                sum = sum + rating_u.value * rating_v.value
+                sum += rating_u.value * rating_v.value
     return sum
 
 
-# calculates the absolute value of a vector (a user/item)
 def calculate_abs_vector(ratings):
+    """ calculates the absolute value of a vector (a user/item)"""
+
     sum = 0
     for movie_id, rating in ratings.iteritems():
-        sum = sum + rating.value ** 2
+        sum += rating.value ** 2
     return math.sqrt(sum)
 
-"""
-Cosine similarity:
 
-Standard metric in item-based recommendations.
-Take values from 0 to 1, where values near to 1 indicate strong similarity. 
-Need only to do computing on movies users both have rated, since the dotproduct of other movies are 0
-"""
-def compute_cosine_similarity(u,v):
+def compute_cosine_similarity(u, v):
+    """
+    Cosine similarity:
+
+    Standard metric in item-based recommendations.
+    Take values from 0 to 1, where values near to 1 indicate strong similarity.
+    Need only to do computing on movies users both have rated, since the dotproduct of other movies are 0
+    """
     
     # cosine similarity function
     return calculate_dot_product(u.ratings, v.ratings)/(calculate_abs_vector(u.ratings)*calculate_abs_vector(v.ratings))
 
-"""
-Pearson's correlation coefficient:
 
-Take values from +1 (strong positive correlation) to -1 (strong negative correlation).
-"""
 def compute_pearson_correlation_coefficient(u, v):
+    """
+    Pearson's correlation coefficient:
+
+    Take values from +1 (strong positive correlation) to -1 (strong negative correlation).
+    """
     avg_rating_u = u.get_rating_average()
     avg_rating_v = v.get_rating_average()
 
@@ -48,7 +52,7 @@ def compute_pearson_correlation_coefficient(u, v):
 
     squared_u = 0
     squared_v = 0
-    denomiator = 0
+    denominator = 0
 
     for movie_id, rating in u.ratings.iteritems():
         if v.ratings.has_key(movie_id):
@@ -56,19 +60,20 @@ def compute_pearson_correlation_coefficient(u, v):
             squared_u += (rating.value - avg_rating_u) ** 2
             squared_v += (v.ratings.get(movie_id).value - avg_rating_v) ** 2
 
-    denomiator += math.sqrt(squared_u) * math.sqrt(squared_v)
+    denominator += math.sqrt(squared_u) * math.sqrt(squared_v)
 
-    if denomiator == 0: 
+    if denominator == 0:
         return 0 
     else: 
-        return numerator / denomiator
+        return numerator / denominator
 
-"""
-Spearman's correlation coefficient:
 
-Take values from +1 (strong positive correlation) to -1 (strong negative correlation).
-"""
 def compute_spearman_correlation_coefficient(u, v):
+    """
+    Spearman's correlation coefficient:
+
+    Take values from +1 (strong positive correlation) to -1 (strong negative correlation).
+    """
     avg_rating_u = u.get_rating_average()
     avg_rating_v = v.get_rating_average()
 
@@ -79,7 +84,7 @@ def compute_spearman_correlation_coefficient(u, v):
     denomiator = 0
 
     for movie_id, rating in u.ratings.iteritems():
-        if v.ratings.has_key(movie_id):
+        if movie_id in v.ratings:
             numerator += (rating.value - avg_rating_u) * (v.ratings.get(movie_id).value - avg_rating_v)
             squared_u += (rating.value - avg_rating_u) ** 2
             squared_v += (v.ratings.get(movie_id).value - avg_rating_v) ** 2
@@ -91,21 +96,22 @@ def compute_spearman_correlation_coefficient(u, v):
     else: 
         return numerator / denomiator
 
-"""
-Mean Squared Difference (MSD):
 
-Does not capture negative correlations between users.
-"""
-def compute_mean_squeared_difference(u, v):
-    denomiator = 0
+def compute_mean_squared_difference(u, v):
+    """
+    Mean Squared Difference (MSD):
+
+    Does not capture negative correlations between users.
+    """
+    denominator = 0
     number_of_corated_items = 0
 
     for movie_id, rating in u.ratings.iteritems():
-        if v.ratings.has_key(movie_id):
-            denomiator += (rating.value - v.ratings.get(movie_id).value) ** 2
+        if movie_id in v.ratings:
+            denominator += (rating.value - v.ratings.get(movie_id).value) ** 2
             number_of_corated_items += 1
     
-    if denomiator == 0:
-        return denomiator
+    if denominator == 0:
+        return denominator
     else:
         return number_of_corated_items
