32ff58fb2548b3d3990364ce7e9622bbb78eb795
Dag Einar Monsen <me@dag.im>
2013-10-01 11:34:53 +0200

    Fix cross validation code.

--- src/evaluation/predictiveAccuracy.py
+++ src/evaluation/predictiveAccuracy.py
@@ -2,11 +2,20 @@ from math import sqrt
 from collections import deque
 
 
+def _chunks(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+    return out
+
+
 def cross_validate(ratings, recommender):
     """ do a cross validation on recommendation algorithm """
     num_folds = 10
-
-    _chunks = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
 
     # divide items into 10 parts
     folds = _chunks(ratings, num_folds)
@@ -19,13 +28,13 @@ def cross_validate(ratings, recommender):
 
         # predict rating for each item in the fold
         for rating in fold:
-            prediction = recommender.get_rating(rating.user, rating.item)
+            prediction = recommender.get_rating(rating.user, rating.movie)
             pairs.append((rating.value, prediction))
 
         queue.append(fold)
 
     # return average error
-    return mean_absolute_error(pairs, range(1, 5))
+    return mean_absolute_error(pairs, range(1, 6))
 
 
 def mean_absolute_error(ratings, range):
