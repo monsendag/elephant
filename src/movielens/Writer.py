11a4f8e8360e3fb270b35337d33f5183045207b9
Dag Einar Monsen <me@dag.im>
2013-10-02 14:26:53 +0200

    Reformat movielens.Writer.

--- src/movielens/Writer.py
+++ src/movielens/Writer.py
@@ -3,12 +3,12 @@ import calendar
 delimiter = '::'
 
 
-# UserID::Gender::Age::Occupation::Zip-code
 def write_users(file, users):
-    with open (file, 'w') as f:
+    """ UserID::Gender::Age::Occupation::Zip-code """
+    with open(file, 'w') as f:
         for user in users:
             # create data array
-            data = 5*[None]
+            data = 5 * [None]
             data[0] = user.id
             data[1] = "M" if user.male else "F"
             data[2] = user.age
@@ -17,30 +17,32 @@ def write_users(file, users):
 
             data = map(str, data)
             # append to file
-            f.write ("::".join(data)+'\n')
+            f.write("::".join(data) + '\n')
         f.close()
 
-# MovieID::Title::Genres
+
 def write_movies(file, movies):
-    with open (file, 'w') as f:
+    """ MovieID::Title::Genres """
+    with open(file, 'w') as f:
         for movie in movies:
             # create data array
-            data = 3*[None]
+            data = 3 * [None]
             data[0] = movie.id
             data[1] = movie.title
             data[2] = "|".join(movie.genres)
 
             data = map(str, data)
             # append to file
-            f.write ("::".join(data)+'\n')
-        f.close();
+            f.write("::".join(data) + '\n')
+        f.close()
 
-# UserID::MovieID::Rating::Timestamp
+
 def write_ratings(file, ratings):
-    with open (file, 'w') as f:
+    """ UserID::MovieID::Rating::Timestamp """
+    with open(file, 'w') as f:
         for rating in ratings:
             # create data array
-            data = 4*[None]
+            data = 4 * [None]
             data[0] = rating.user.id
             data[1] = rating.movie.id
             data[2] = rating.value
@@ -48,6 +50,6 @@ def write_ratings(file, ratings):
 
             data = map(str, data)
             # append to file
-            f.write ("::".join(data)+'\n')
-        f.close();
+            f.write("::".join(data) + '\n')
+        f.close()
 
