a4f4296760a7ac4a79d397eb554aed950c4e5419
Dag Einar Monsen <me@dag.im>
2013-10-02 17:26:44 +0200

    Reflect changes in Recommender API. Disable evaluation.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ datastore.add_users(movielens.load_users(path.get('data/sample100/users.dat')))
 datastore.add_items(movielens.load_movies(path.get('data/sample100/movies.dat')))
 datastore.add_ratings(movielens.load_ratings(path.get('data/sample100/ratings.dat')))
 
+
 from src.recommenders import SimpleKNN
 
 SimpleKNN.add_users(datastore.get_users())
@@ -18,14 +19,12 @@ SimpleKNN.add_items(datastore.get_items())
 
 # get random user
 user = datastore.get_random_user()
-recommendations = SimpleKNN.get_recommendations(user)
-print recommendations
+recommendations = SimpleKNN.get_recommendations(user, 10)
+
 sorted = reversed(sorted(recommendations.items(), key=lambda (k, v): v))
 
 print 'Movie id     Rating      Movie title'
 for (r,rating) in sorted:
     print r.id, '             ', "{0:.2f}".format(rating), '      ', r.title
 
-import scripts
 
-scripts.time_get_rating()
