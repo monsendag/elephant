597bbe507132dd382c358de53ed79e3f9e074560
Patrick Romstad <promstad1@gmail.com>
2013-10-03 10:26:05 +0200

    Written better description for get_recommendations

--- src/recommenders/SimpleKNN.py
+++ src/recommenders/SimpleKNN.py
@@ -23,27 +23,30 @@ def get_recommendations(user, num):
     Recommendation algorithm - user-based nearest neighbor recommendation:
 
     Computes recommendation based on the neighborhood of the active user.
-    The similarity between users can the computed with different similarity metrics,
-    and the similarity metric is decided in step (1)
-    It is also different ways of producing the recommendations, and this is decided
-    in step (3)
+    Works in three steps: 
+        1. Compute similarities for all users with respect to the active user
+        2. Select a subset to use as a set of predictions (neighborhood)
+        3. Compute predictions based on the neighborhood
+
+    Step 1 and 2 is done together in # (1), while step 3 is done in # (2). 
+    The third parameter in get_closest_neighbors in # (1) decides how to compute the similarities
 
     """
     global _users
     global _items
 
-    # (2) Create the neighborhood of the 10 closest users
+    # (1) Create the neighborhood of the 10 closest users
     neighbors = get_closest_neighbors(user, 10, SimilarityMetrics.compute_pearson_correlation_coefficient)
 
-    # (3) Compute predictions
+    # (2) Compute predictions
     predictions = {}
     for item in _items:
         predictions[item] = ProduceRecommendation.prediction_based(user, item, neighbors)
 
-    # get n highest rated items based on predicted rating
+    # (3) get n highest rated items based on predicted rating (top-n recommendation)
     highest_rated_arr = nlargest(num, predictions, key=lambda k: predictions[k])
 
-    # create dictionary with item:rating pairs
+    # (4) create dictionary with item:rating pairs
     highest_rated = {}
     for item in highest_rated_arr:
         highest_rated[item] = predictions[item]
