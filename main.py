9c9a92471e027ee8709b7ac3ce6b4a1197b9a36e
Dag Einar Monsen <me@dag.im>
2013-10-03 15:24:00 +0200

    Rename src.path to src.util and add timer method.

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ parser.add_argument('-e', '--evaluate', help='Evaluate algorithm', required=Fals
 args = parser.parse_args()
 
 from src import datastore
-from src import path
+from src import util
 
 # movielens specific data
 from src import movielens
 
+
+
 # load data into datastore
-datastore.add_users(movielens.load_users(path.get('data/sample100/users.dat')))
-datastore.add_items(movielens.load_movies(path.get('data/sample100/movies.dat')))
-datastore.add_ratings(movielens.load_ratings(path.get('data/sample100/ratings.dat')))
-
-
+datastore.add_users(movielens.load_users(util.path('data/sample100/users.dat')))
+datastore.add_items(movielens.load_movies(util.path('data/sample100/movies.dat')))
+datastore.add_ratings(movielens.load_ratings(util.path('data/sample100/ratings.dat')))
 from src.recommenders import SimpleKNN
 
 SimpleKNN.add_users(datastore.get_users())
