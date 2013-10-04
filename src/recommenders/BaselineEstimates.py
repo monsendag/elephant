65fa016f2d4e06a05ef66bf88254e6c67aecf81e
Dag Einar Monsen <me@dag.im>
2013-10-04 15:34:27 +0200

    Fix indentation in BaselineEstimates.

--- src/recommenders/BaselineEstimates.py
+++ src/recommenders/BaselineEstimates.py
@@ -4,24 +4,24 @@ It is used to adjust predictions for items that generally have higher or lower r
 than others, or adjust predictions for users that generally rates higher or lower than others.
 
 Formula:
-	b_ui = r_avg + b_u + b_i
+    b_ui = r_avg + b_u + b_i
 
-		b_ui = baseline estimate for an unknown rating r_ui (user u and item i)
-		r_avg = average overall rating
-		b_u = observed deviations of user u
-		b_i = observed deviations of item i
+        b_ui = baseline estimate for an unknown rating r_ui (user u and item i)
+        r_avg = average overall rating
+        b_u = observed deviations of user u
+        b_i = observed deviations of item i
 
-	b_i =	( for all known ratings r_ui, u : (r_ui - r_avg) ) / 
-			( y_1 - number of different users that have given a rating )
+    b_i = ( for all known ratings r_ui, u : (r_ui - r_avg) ) /
+            ( y_1 - number of different users that have given a rating )
 
-	b_u = 	( for all known ratings r_ui, i : (r_ui - r_avg - b_i) ) / 
-			( y_2 + number of different items that have been rated )
+    b_u = ( for all known ratings r_ui, i : (r_ui - r_avg - b_i) ) /
+            ( y_2 + number of different items that have been rated )
 
-	   y_1 and y_2 are regularization parameters and are determined by cross validation. Typical values
-	   in the Netflix dataset are: y_1 = 25, y_2 = 10
+       y_1 and y_2 are regularization parameters and are determined by cross validation. Typical values
+       in the Netflix dataset are: y_1 = 25, y_2 = 10
 
 Source: Koren, Y. 2010. Factor in the neighbors: Scalable and accurate collaborative filtering. ACM Trans.
-		Knowl. Discov. Data. 4, 1, Article 1 (January 2010), 24 pages.
+        Knowl. Discov. Data. 4, 1, Article 1 (January 2010), 24 pages.
 """
 from src import datastore
 
